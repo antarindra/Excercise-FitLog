a8e2fd034db4f7092fb7d7cdfa072a1ee3dcb485
antar-web <antar.22cse032@gstu.edu.bd>
2026-09-25 14:31:08 +0600

    fixed image hostname

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname:'img.magnific.com',
         
       },
     ],
